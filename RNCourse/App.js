091df245ef34d12d34d4008b72c31674d6c08bc8
nickguerra <nickg@maliciousgames.com>
2022-08-18 05:55:32 -0500

    Added basic styles

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Keller Car</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Another Text</Text>
+      </View>
+      <Text 
+      style={styles.dummyText}
+      >Hello Keller
+      </Text>
+      <Button title='Tap Me!' />
     </View>
   );
 }
@@ -17,4 +23,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16, 
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue'
+  }
 });
