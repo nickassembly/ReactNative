a9dc2a84cd2188743326bb0f4f6b7414846c5d16
nickguerra <nickg@maliciousgames.com>
2022-08-19 06:02:12 -0500

    Added flexbox to view

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,32 +1,33 @@
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Another Text</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
+        <Button title="Add Goal" />
       </View>
-      <Text 
-      style={styles.dummyText}
-      >Hello Keller
-      </Text>
-      <Button title='Tap Me!' />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
   },
-  dummyText: {
-    margin: 16, 
-    padding: 16,
-    borderWidth: 2,
-    borderColor: 'blue'
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
   }
 });
